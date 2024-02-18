38778fffcba2239bde9a8272e321c61cb14b5448
Thomas Mazzotta <thomasmazzotta99@gmail.com>
2024-02-18 16:27:08 +0100

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+
+// this is the entry point of the entire application
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
